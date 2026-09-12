a8cb749dc8c4bd10bbea9174aca8d680afe2a693
Toukennn <amirrezakhatibi2009@gmail.com>
2026-09-12 17:21:52 +0330

    fixing a type in main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from agents.data_engineer import data_engineer
 from langchain_core.messages import HumanMessage
 
 if __name__ == "__main__":
-    response = data_agent.invoke(
+    response = data_engineer.invoke(
         {"messages":[HumanMessage(content="I want to extract the data from the API endpoint 'https://pokeapi.co/api/v2/pokemon' and save it to data/extract folder in the csv folder")],
          "route_response": ""}
     )
